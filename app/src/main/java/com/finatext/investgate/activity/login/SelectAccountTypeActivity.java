d5afdbcfc7bc26599ce066b9193b2689b490ca03
vfa-kitvs <kitvs@vitalify.asia>
2016-06-17 17:49:41 +0700

    add temp screen

---
+++ app/src/main/java/com/finatext/investgate/activity/login/SelectAccountTypeActivity.java
@@ -0,0 +1,18 @@
+package com.finatext.investgate.activity.login;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.finatext.investgate.R;
+import com.finatext.investgate.activity.BaseActivity;
+
+/**
+ * Created by apple on 6/17/16.
+ */
+public class SelectAccountTypeActivity extends BaseActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_select_account_type);
+    }
+}
