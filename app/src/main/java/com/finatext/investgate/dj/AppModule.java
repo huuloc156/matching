de0b943ca3625c9c796cc1d1185cca97f1db6156
kitvong <kitvong@executionlab.asia>
2016-06-21 14:52:48 +0700

    configure request header, add token info

--- app/src/main/java/com/finatext/investgate/dj/AppModule.java
+++ app/src/main/java/com/finatext/investgate/dj/AppModule.java
@@ -2,6 +2,9 @@ package com.finatext.investgate.dj;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
 
 import com.finatext.investgate.BuildConfig;
 import com.finatext.investgate.R;
@@ -10,11 +13,15 @@ import com.finatext.investgate.data.api.InvestgateApi;
 import com.squareup.otto.Bus;
 import com.squareup.otto.ThreadEnforcer;
 
+import java.io.IOException;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -42,7 +49,7 @@ public class AppModule {
 
     @Provides
     @Singleton
-    OkHttpClient provideOkHttpClient() {
+    OkHttpClient provideOkHttpClient(final Context context, final SharePreferenceData preferenceData) {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         if(BuildConfig.DEBUG) {
             HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
@@ -51,6 +58,32 @@ public class AppModule {
             httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
             builder.networkInterceptors().add(httpLoggingInterceptor);
         }
+        // Define the interceptor, add authentication headers
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request.Builder newBuilder = chain.request().newBuilder();
+                String android_id = Settings.Secure.getString(context.getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+                String versionName = "";
+                try {
+                    versionName = context.getPackageManager()
+                            .getPackageInfo(context.getPackageName(), 0).versionName;
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+                //request fx header
+                newBuilder.addHeader("X-Investgate-Client-OS", "android");
+                newBuilder.addHeader("X-Investgate-Client-OS-Version", Build.VERSION.RELEASE);
+                newBuilder.addHeader("X-Investgate-Client-Device", Build.MODEL);
+                newBuilder.addHeader("X-Investgate-Client-Device-ID", android_id);
+                newBuilder.addHeader("X-Investgate-Client-App-Version", versionName);
+                //session
+                newBuilder.addHeader("X-Investgate-Token",preferenceData.getUserToken());
+                return chain.proceed(newBuilder.build());
+            }
+        };
+        builder.addInterceptor(interceptor);
         return builder.build();
     }
     @Singleton
