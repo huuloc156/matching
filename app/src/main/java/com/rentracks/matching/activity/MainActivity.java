54352da82de204f0ac984fe41c32a8623bcea04e
HuuLoc <huuloc.ce@gmail.com>
2017-06-20 18:41:15 +0700

    import libaries intant

--- app/src/main/java/com/rentracks/matching/activity/MainActivity.java
+++ app/src/main/java/com/rentracks/matching/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rentracks.matching.activity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
